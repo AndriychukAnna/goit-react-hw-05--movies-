3459c5b4d5a1fb1ac7d9ad521315d7c1bc8a0c2c
AndriychukAnna <andriychykal98@gmail.com>
2021-10-23 22:48:34 +0300

    netlify add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./Components/APP";
+import App from "./components/APP/index";
 import "./index.css";
 import "modern-normalize/modern-normalize.css";
 
